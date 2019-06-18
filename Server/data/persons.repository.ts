8261147519817d1c08c5cf1dfe188ac95af39f1b
swagath <swagathbairi@gmail.com>
2019-06-19 01:42:42 +0530

    PersonsSchema CRUD

--- Server/data/persons.repository.ts
+++ Server/data/persons.repository.ts
@@ -1,36 +1,44 @@
 import { MongoRepository } from "./_mongo.repository";
 import { person, IPerson } from '../schemas/persons.schema'
+import { ObjectID } from "bson";
+import { NativeError } from "mongoose";
 
 export class PersonsRepository extends MongoRepository {
     constructor() {
         super("persons");
     }
 
-    async findOne(name: string): Promise<IPerson | null> {
-        var p = person.findOne({ "name": name })
-        p.sort("name")
-        return p.exec((err, person) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            return person
-        })
+    async findOne(query: any): Promise<IPerson | null | Error> {
+        var p = person.findOne(query)
+        try {
+            let result = await p.exec();
+            return result;
+        } catch (err) {
+            console.log(err);
+            return new Error(err.message);
+        }
     }
 
-    async createPerson(): Promise<any> {//Promise<IPerson|null>{
-        let a = new person({ firstName: "asd", lastName: "Bairi", email: "swafasda@asd.com", mobile: 9014720192 });
-        a.firstName = "Swagath";
-        let asd = await person.create(a, (err: any, res: any) => {
-            if (err) {
-                console.log(err);
-                return err;
-            }
-            else {
-                console.log(res);
-                return res;
-            }
-        });
+    async createPerson(p:IPerson): Promise<IPerson|Error> {
+        try {
+            let result = await person.create(p);
+            return result;
+        } catch (err) {
+            return new Error("Error while saving person to DB")
+        }
+
     }
+
+    async findPersonById(id: string): Promise<IPerson | null| Error> {
+        var p = person.findById(id.toString());
+        try{
+            let result = await p.exec();
+            return result;
+        }catch(err){
+            console.log(err);
+            return new Error(err.message);
+        }
+    }
+
 
 }
